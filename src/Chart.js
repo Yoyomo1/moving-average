6f4c6368475020f27666c839e8962cd238af4b26
Marcus <marcusgchan35@gmail.com>
2021-08-04 22:02:15 -0700

    Finish basic chart implementation

--- src/Chart.js
+++ src/Chart.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Line } from "react-chartjs-2";
 
-const Chart = ({ stockData }) => {
+const Chart = ({ stockData, dates, movingAverage }) => {
   const data = {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+    labels: dates,
     datasets: [
       {
+        label: "Price",
+        data: stockData,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+      {
         label: "Moving Average",
-        data: [12, 19, 3, 5, 2, 3],
+        data: movingAverage,
         backgroundColor: [
           "rgba(255, 99, 132, 0.2)",
           "rgba(54, 162, 235, 0.2)",
@@ -28,18 +49,6 @@ const Chart = ({ stockData }) => {
       },
     ],
   };
-
-  // Returns array of objects
-  const parseData = (data) => {
-    data = data["Time Series (Daily)"];
-
-    if (data) {
-      let arr = Object.entries(data);
-      console.log(arr);
-    }
-  };
-
-  const stocksObjArray = parseData(stockData);
 
   return (
     <div className="chart-container">
--- src/Content.js
+++ src/Content.js
@@ -3,7 +3,11 @@ import Chart from "./Chart";
 import { useState, useEffect } from "react";
 
 const Content = ({ currentSymbol }) => {
-  const [stockData, setStockData] = useState({});
+  const [stockData, setStockData] = useState([]);
+  const [stockData59Days, setStockData59Days] = useState([]);
+  const [listOfDates, setListOfDates] = useState([]);
+  const [movingAverage, setMovingAverage] = useState([]);
+
   const baseURL = "https://www.alphavantage.co/query?";
   const params = {
     f: "TIME_SERIES_DAILY_ADJUSTED",
@@ -11,17 +15,51 @@ const Content = ({ currentSymbol }) => {
     apiKey: "IBA86A2CGCBIGGHP",
   };
 
+  const parseData = (data) => {
+    data = data["Time Series (Daily)"];
+
+    if (data) {
+      let arr = Object.entries(data);
+
+      // Get array of 30 dates
+      const dates = arr.map((subArr) => subArr[0]);
+      const dates30Days = dates.slice(0, 30);
+      setListOfDates(dates30Days);
+
+      arr = arr.map((element) => element[1]["5. adjusted close"]);
+      let dataFor30Days = arr.slice(0, 30);
+      setStockData59Days(arr.slice(0, 59));
+      return dataFor30Days;
+    }
+  };
+
+  const set30DayMovingAvg = () => {
+    let movingAvg = [];
+    let sum = 0;
+    for (let i = 0; i < 30; i++) {
+      for (let j = 0; j < 30; j++) {
+        sum += parseInt(stockData59Days[i + j]);
+      }
+      movingAvg.push(sum / 30.0);
+      sum = 0;
+    }
+
+    setMovingAverage(movingAvg);
+  };
+
   useEffect(() => {
     fetch(
-      // `${baseURL}function=${params.f}&symbol=${params.symbol}&apikey=${params.apiKey}`
-      "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo"
+      `${baseURL}function=${params.f}&symbol=${params.symbol}&apikey=${params.apiKey}`
+      // "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=IBM&apikey=demo"
     )
       .then((response) => {
         if (!response.ok) throw Error(response.statusText);
         return response.json();
       })
       .then((data) => {
-        setStockData(data);
+        const stockData = parseData(data);
+        setStockData(stockData);
+        set30DayMovingAvg();
       })
       .catch((error) => console.log(error));
   }, []);
@@ -29,7 +67,12 @@ const Content = ({ currentSymbol }) => {
   return (
     <section className="content-container">
       <h3>{currentSymbol}</h3>
-      <Chart stockData={stockData} />
+      <Chart
+        stockData={stockData}
+        dates={listOfDates}
+        closingPricesFor59Days={stockData59Days}
+        movingAverage={movingAverage}
+      />
     </section>
   );
 };
